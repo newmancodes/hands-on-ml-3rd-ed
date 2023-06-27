7940e573d49240da85745d53f3fb8329c923f1ba
Stephen Newman <steve@newman.digital>
2023-06-27 08:13:08 +0100

    Load housing data.

---
+++ california-housing-prices/end_to_end_machine_learning_project.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+assert sys.version_info >= (3, 7)
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing = load_housing_data()
+housing.info()
