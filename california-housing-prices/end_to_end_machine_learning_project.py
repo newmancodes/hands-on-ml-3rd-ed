4fe34c0342cdfb07ce1f7d1b7f42befe0a3f7355
Stephen Newman <steve@newman.digital>
2023-06-27 09:08:47 +0100

    Add pylint and another way to generate the training and test datasets.

--- california-housing-prices/end_to_end_machine_learning_project.py
+++ california-housing-prices/end_to_end_machine_learning_project.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sys
 import tarfile
 import urllib.request
+from zlib import crc32
 
 assert sys.version_info >= (3, 7)
 
@@ -18,12 +19,28 @@ def load_housing_data():
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
 def shuffle_and_split_data(data, test_ratio):
+    # Splits the supplied data into training and a test set of data in accordance with the supplied ratio
+    # This approach has downsides as it is random, meaning that each time this split is performed
+    # different records will appear in the training or test sets.
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
+def is_id_in_test_set(identifier, test_ratio):
+    return crc32(np.int64(identifier)) < test_ratio * 2**32
+
+def split_data_with_id_hash(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
+    return data.loc[-in_test_set], data.loc[in_test_set]
+
 housing = load_housing_data()
-train_set, test_set = shuffle_and_split_data(housing, 0.2)
+
+# train_set, test_set = shuffle_and_split_data(housing, 0.2)
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "index")
+
 print(f'{len(train_set)} houses in the training set, {len(test_set)} houses in the test set.')
